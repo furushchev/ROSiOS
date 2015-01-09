f5c7e97935399bdc792efdba156cb707b1a4b86a
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-01-09 16:02:50 +0900

    Initial Commit

---
+++ ROSiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ROSiOS
+//
+//  Created by FurutaYuki on 1/9/15.
+//  Copyright (c) 2015 JSK Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ROSiOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ROSiOS
+//
+//  Created by FurutaYuki on 1/9/15.
+//  Copyright (c) 2015 JSK Lab. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
